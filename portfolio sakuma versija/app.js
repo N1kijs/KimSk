d3eaf2a66c4412307e6e149b8c66b402d37e404a
N1kijs <48949270+N1kijs@users.noreply.github.com>
2020-05-05 13:35:06 +0300

    Add files via upload

---
+++ portfolio sakuma versija/app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() { 
+
+    $("#header").css("background", "#707070");
+    $("#header").css("width", "100%");
+    $("#header").css("height", "180px");
+
+    $("#body").css("background", "#454545");
+    $("#body").css("width", "100%");
+    $("#body").css("height", "59%");
+    
+    $("#navigation_header").css("padding-top", "20px");
+    $("#navigation_header").css("padding-bottom", "20px");
+
+    $("#body_content").css("background", "#454545");
+    $("#body_content").css("padding-top", "50px");
+    $("#body_content").css("padding-left", "150px");
+    $("#body_content").css("padding-bottom", "285px");
+    $("#body_content").css("color", "white");
+
+    $("#table").css("background", "#454545");
+    $("#table").css("padding-top", "50px");
+    $("#table").css("padding-left", "150px");
+    $("#table").css("color", "white");
+
+    $("#input").css("background", "#454545");
+    $("#input").css("padding-top", "50px");
+    $("#input").css("padding-left", "150px");
+    $("#input").css("color", "white");
+});
